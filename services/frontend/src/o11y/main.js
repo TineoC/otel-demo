7e1ec942939fe700c91b52d530cd3fa83014a0d2
Christopher Tineo <christophertineo02@gmail.com>
2025-05-18 01:27:05 -0400

    update deploy

--- services/frontend/src/o11y/main.js
+++ services/frontend/src/o11y/main.js
@@ -5,13 +5,13 @@ import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { B3Propagator } from '@opentelemetry/propagator-b3';
+import { SEMRESATTRS_R } from '@opentelemetry/semantic-conventions';
 
 // create the OTLP/Collector exporter
 const collectorUrl = 'v1/traces'
 
 const exporter = new CollectorTraceExporter({
   url: collectorUrl,
-  serviceName: 'store-frontend',
 });
 
 
@@ -20,6 +20,9 @@ const provider = new WebTracerProvider({
   spanProcessors: [
     new SimpleSpanProcessor(exporter),
   ],
+  resource: {
+    [SEMRESATTRS_R.SERVICE_NAME]: 'store-frontend',
+  }
 });
 
 // register context propagation and manager
