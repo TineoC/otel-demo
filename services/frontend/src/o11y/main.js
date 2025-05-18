7753701261e16e4256796edbebac273f79578bfb
Christopher Tineo <christophertineo02@gmail.com>
2025-05-18 12:31:07 -0400

    refactor frontend traces exporter

--- services/frontend/src/o11y/main.js
+++ services/frontend/src/o11y/main.js
@@ -1,17 +1,16 @@
+import {
+  // ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
-import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
-import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
-import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
-import { B3Propagator } from '@opentelemetry/propagator-b3';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { CompositePropagator, W3CTraceContextPropagator } from '@opentelemetry/core';
+import { B3Propagator } from '@opentelemetry/propagator-b3';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-
-const exporter = new CollectorTraceExporter({
-  url: 'http://localhost:4318/v1/traces',
-});
-
 
 const resource = resourceFromAttributes({
   [ATTR_SERVICE_NAME]: 'store-frontend',
@@ -19,24 +18,38 @@ const resource = resourceFromAttributes({
 
 // set up the tracer provider with a simple span processor
 const provider = new WebTracerProvider({
+  resource,
   spanProcessors: [
-    new SimpleSpanProcessor(exporter),
+    // new SimpleSpanProcessor(new ConsoleSpanExporter()),
+    new SimpleSpanProcessor(new OTLPTraceExporter()),
   ],
-  resource
 });
 
 // register context propagation and manager
 provider.register({
   contextManager: new ZoneContextManager(),
-  propagator: new B3Propagator(),
+  propagator: new CompositePropagator({
+    propagators: [
+      new B3Propagator(),
+      new W3CTraceContextPropagator(),
+    ],
+  }),
 });
 
-// enable automatic instrumentation of browser APIs
+// Registering instrumentations
 registerInstrumentations({
+  tracerProvider: provider,
   instrumentations: [
     getWebAutoInstrumentations({
+      // load custom configuration for xml-http-request instrumentation
       '@opentelemetry/instrumentation-xml-http-request': {
         clearTimingResources: true,
+      },
+      '@opentelemetry/instrumentation-fetch': {
+        clearTimingResources: true,
+      },
+      '@opentelemetry/instrumentation-user-interaction': {
+        eventNames: ['click', 'submit'],
       },
     }),
   ],
